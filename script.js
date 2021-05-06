7eae6b2ac6d99724d8bddd20e855bd280aeb85d0
SUDHARSHAN S <sudharsh-pt4118@sudharsh-pt4118.csez.zohocorpin.com>
2021-05-06 10:57:34 +0530

    script file

---
+++ script.js
@@ -0,0 +1,14 @@
+const BOARD_WIDTH = 8;
+const BOARD_HEIGHT = 8;
+
+const TILE_SIZE=50;
+const WHITE_TILE_COLOR='rgb(255,228,196)';
+const WHITE_TILE_COLOR = 'rgb(75,175,75)';
+
+const EMPTY = -1;
+const PAWN = 0;
+const KNIGHT = 1;
+const BISHOP = 2;
+const ROOK = 3;
+const QUEEN = 4;
+const KING = 5;
