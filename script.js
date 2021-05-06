3a203b09a539963e98bdf213c8d8f40121f39ad2
SUDHARSHAN S <sudharsh-pt4118@sudharsh-pt4118.csez.zohocorpin.com>
2021-05-06 11:52:13 +0530

    UI completed

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const BOARD_WIDTH = 8;
 const BOARD_HEIGHT = 8;
 
 const TILE_SIZE=50;
-const WHITE_TILE_COLOR='rgb(255,228,196)';
-const WHITE_TILE_COLOR = 'rgb(75,175,75)';
+const WHITE_TILE_COLOR = "rgb(255, 228, 196)";
+const BLACK_TILE_COLOR = "rgb(206, 162, 128)";
+const HIGHLIGHT_COLOR = "rgb(75, 175, 75)";
+const WHITE = 0;
+const BLACK = 1;
 
 const EMPTY = -1;
 const PAWN = 0;
@@ -12,3 +15,175 @@ const BISHOP = 2;
 const ROOK = 3;
 const QUEEN = 4;
 const KING = 5;
+
+
+const INVALID = 0;
+const VALID = 1;
+const VALID_CAPTURE = 2;
+
+const piecesCharacters = {
+    0: '♙',
+    1: '♘',
+    2: '♗',
+    3: '♖',
+    4: '♕',
+    5: '♔'
+};
+let chessCanvas;
+let chessCtx;
+let currentTeamText;
+let whiteCasualitiesText;
+let blackCasualitiesText;
+let totalVictoriesText;
+
+let board;
+let currentTeam;
+
+let curX;
+let curY;
+
+let whiteCasualities;
+let blackCasualities;
+
+let whiteVictories;
+let blackVictories;
+document.addEventListener("DOMContentLoaded",onLoad);
+function onLoad()
+{
+    chessCanvas=document.getElementById('chessCanvas');
+    console.log(chessCanvas);
+    chessCtx=chessCanvas.getContext("2d");
+    currentTeamText=document.getElementById('currentTeamText')
+    whiteCasualitiesText = document.getElementById('whiteCasualities')
+    blackCasualitiesText= document.getElementById('blackCasualities')
+    totalVictoriesText = document.getElementById('totalVictories');
+    whiteVictoriesText=0;
+    blackVictories=0;
+    startGame();
+}
+function startGame()
+{
+    board=new Board();
+    curX=-1;
+    curY=-1;
+    currentTeam=WHITE;
+    currentTeamText.textContent=`White's turn`;
+    whiteCasualities=[0,0,0,0,0];
+    blackCasualities = [0, 0, 0, 0, 0];
+    repaintBoard();
+
+  //  console.log(board);
+}
+
+function repaintBoard() {
+    drawBoard();
+    checkPossiblePlays();
+    drawPieces();
+}
+function drawBoard() {
+    chessCtx.fillStyle = WHITE_TILE_COLOR;
+    chessCtx.fillRect(0, 0, BOARD_WIDTH * TILE_SIZE, BOARD_HEIGHT * TILE_SIZE);
+
+    for (let i = 0; i < BOARD_HEIGHT; i++) {
+        for (let j = 0; j < BOARD_WIDTH; j++) {
+            if ((i + j) % 2 === 1) {
+                drawTile(j, i, BLACK_TILE_COLOR);
+            }
+        }
+    }
+}
+function drawTile(x, y, fillStyle) {
+    chessCtx.fillStyle = fillStyle;
+    chessCtx.fillRect(TILE_SIZE * x, TILE_SIZE * y, TILE_SIZE, TILE_SIZE);
+}
+class Board {
+    constructor() {
+        this.tiles = [];
+
+        this.tiles.push([
+            new Tile(ROOK, BLACK),
+            new Tile(KNIGHT, BLACK),
+            new Tile(BISHOP, BLACK),
+            new Tile(QUEEN, BLACK),
+            new Tile(KING, BLACK),
+            new Tile(BISHOP, BLACK),
+            new Tile(KNIGHT, BLACK),
+            new Tile(ROOK, BLACK)
+        ]);
+
+        this.tiles.push([
+            new Tile(PAWN, BLACK),
+            new Tile(PAWN, BLACK),
+            new Tile(PAWN, BLACK),
+            new Tile(PAWN, BLACK),
+            new Tile(PAWN, BLACK),
+            new Tile(PAWN, BLACK),
+            new Tile(PAWN, BLACK),
+            new Tile(PAWN, BLACK)
+        ]);
+
+        for (let i = 0; i < 4; i++) {
+            this.tiles.push([
+                new Tile(EMPTY, EMPTY),
+                new Tile(EMPTY, EMPTY),
+                new Tile(EMPTY, EMPTY),
+                new Tile(EMPTY, EMPTY),
+                new Tile(EMPTY, EMPTY),
+                new Tile(EMPTY, EMPTY),
+                new Tile(EMPTY, EMPTY),
+                new Tile(EMPTY, EMPTY),
+            ]);
+        }
+
+        this.tiles.push([
+            new Tile(PAWN, WHITE),
+            new Tile(PAWN, WHITE),
+            new Tile(PAWN, WHITE),
+            new Tile(PAWN, WHITE),
+            new Tile(PAWN, WHITE),
+            new Tile(PAWN, WHITE),
+            new Tile(PAWN, WHITE),
+            new Tile(PAWN, WHITE)
+        ]);
+
+        this.tiles.push([
+            new Tile(ROOK, WHITE),
+            new Tile(KNIGHT, WHITE),
+            new Tile(BISHOP, WHITE),
+            new Tile(QUEEN, WHITE),
+            new Tile(KING, WHITE),
+            new Tile(BISHOP, WHITE),
+            new Tile(KNIGHT, WHITE),
+            new Tile(ROOK, WHITE)
+        ]);
+
+        this.validMoves = [];
+        for (let i = 0; i < BOARD_HEIGHT; i++) {
+            this.validMoves.push([
+                INVALID,
+                INVALID,
+                INVALID,
+                INVALID,
+                INVALID,
+                INVALID,
+                INVALID,
+                INVALID
+            ]);
+        }
+    }
+
+    resetValidMoves() {
+        for (let i = 0; i < BOARD_HEIGHT; i++) {
+            for (let j = 0; j < BOARD_WIDTH; j++) {
+                this.validMoves[i][j] = INVALID;
+            }
+        }
+    }
+}
+
+class Tile {
+    constructor(pieceType, team) {
+        this.pieceType = pieceType;
+        this.team = team;
+    }
+}
