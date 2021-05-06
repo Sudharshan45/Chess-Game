28e3c8412c259a6748321a0df1ecbdf823b7a3e4
SUDHARSHAN S <sudharsh-pt4118@sudharsh-pt4118.csez.zohocorpin.com>
2021-05-06 19:13:06 +0530

    new UI

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const BOARD_WIDTH = 8;
 const BOARD_HEIGHT = 8;
 
-const TILE_SIZE=50;
-const WHITE_TILE_COLOR = "rgb(255, 228, 196)";
-const BLACK_TILE_COLOR = "rgb(206, 162, 128)";
-const HIGHLIGHT_COLOR = "rgb(75, 175, 75)";
 const WHITE = 0;
 const BLACK = 1;
 
@@ -22,14 +18,14 @@ const VALID = 1;
 const VALID_CAPTURE = 2;
 
 const piecesCharacters = {
-    0: '♙',
-    1: '♘',
-    2: '♗',
-    3: '♖',
-    4: '♕',
-    5: '♔'
+    0: 'Pawn.png',
+    1: 'Knight.png',
+    2: 'Bishop.png',
+    3: 'Rook.png',
+    4: 'Queen.png',
+    5: 'King.png'
 };
-let chessCanvas;
+let chessBox;
 let chessCtx;
 let currentTeamText;
 let whiteCasualitiesText;
@@ -48,53 +44,68 @@ let blackCasualities;
 let whiteVictories;
 let blackVictories;
 document.addEventListener("DOMContentLoaded",onLoad);
+
 function onLoad()
 {
-    chessCanvas=document.getElementById('chessCanvas');
-    console.log(chessCanvas);
-    chessCtx=chessCanvas.getContext("2d");
-    currentTeamText=document.getElementById('currentTeamText')
-    whiteCasualitiesText = document.getElementById('whiteCasualities')
-    blackCasualitiesText= document.getElementById('blackCasualities')
-    totalVictoriesText = document.getElementById('totalVictories');
-    whiteVictoriesText=0;
-    blackVictories=0;
+     chessBox=document.querySelectorAll('.piece-box');
+    console.log(chessBox);
+    for(let i=0;i<chessBox.length;i++)
+    {
+     chessBox[i].addEventListener('click',onClick);
+    }
+    whiteVictories = 0;
+    blackVictories = 0;
+
     startGame();
 }
 function startGame()
 {
-    board=new Board();
-    curX=-1;
-    curY=-1;
-    currentTeam=WHITE;
-    currentTeamText.textContent=`White's turn`;
-    whiteCasualities=[0,0,0,0,0];
+    board = new Board();
+    curX = -1;
+    curY = -1;
+     console.log(board);
+    currentTeam = WHITE;
+   // currentTeamText.textContent = "White's turn";
+
+    whiteCasualities = [0, 0, 0, 0, 0];
     blackCasualities = [0, 0, 0, 0, 0];
     repaintBoard();
-
-  //  console.log(board);
 }
-
-function repaintBoard() {
-    drawBoard();
-    checkPossiblePlays();
+function repaintBoard()
+{
+  //  drawBoard();
+  //  checkPossiblePlays();
     drawPieces();
 }
-function drawBoard() {
-    chessCtx.fillStyle = WHITE_TILE_COLOR;
-    chessCtx.fillRect(0, 0, BOARD_WIDTH * TILE_SIZE, BOARD_HEIGHT * TILE_SIZE);
-
+function drawPieces() {
     for (let i = 0; i < BOARD_HEIGHT; i++) {
         for (let j = 0; j < BOARD_WIDTH; j++) {
-            if ((i + j) % 2 === 1) {
-                drawTile(j, i, BLACK_TILE_COLOR);
+            let pieceType
+            if (board.tiles[i][j].team === EMPTY) continue;
+
+            if (board.tiles[i][j].team === WHITE) {
+                pieceType = './img/White' +piecesCharacters[board.tiles[i][j].pieceType];
+            } else {
+                pieceType = './img/Black' + piecesCharacters[board.tiles[i][j].pieceType];
             }
+           console.log(i,j);
+            const classes=i.toString()+j.toString()
+        console.log(pieceType);
+          const piece=document.getElementsByClassName(classes);
+          console.log(piece[0]);
+            let pieceImg = document.createElement("IMG");
+            pieceImg.setAttribute('src',pieceType);
+            pieceImg.setAttribute('class','piece')
+           piece[0].appendChild(pieceImg)
+        
+           
         }
     }
 }
-function drawTile(x, y, fillStyle) {
-    chessCtx.fillStyle = fillStyle;
-    chessCtx.fillRect(TILE_SIZE * x, TILE_SIZE * y, TILE_SIZE, TILE_SIZE);
+function onClick()
+{
+   console.log(this);
+    this.style.backgroundColor='red'
 }
 class Board {
     constructor() {
